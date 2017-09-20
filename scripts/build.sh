eb58c59ac9fead46478d739143e5d3f91e4f21b2
Fabian van der Werf <fvanderwerf@gmail.com>
2017-09-20 16:14:41 +0200

    fix ecr get-login

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,8 @@
 
 TAG=$(git rev-parse --short HEAD)
 
-
 docker build -t ${DOCKER_IMAGE}:${TAG} job-docker
 
-`aws ecr get-login`
+`aws ecr get-login --registry-ids ${AWS_ACCOUNT_ID} --no-include-email`
 
 docker push ${DOCKER_IMAGE}:${TAG}
