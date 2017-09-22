c2e9597faa36551b40c8d9cbfc1139cf096a734f
Fabian van der Werf <fvanderwerf@gmail.com>
2017-09-22 22:30:01 +0200

    fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,10 +8,5 @@ docker build -t ${DOCKER_IMAGE}:${TAG} job-docker
 
 docker push ${DOCKER_IMAGE}:${TAG}
 
-if aws cloudformation describe-stacks --stack-name ${CF_STACK_NAME}
-then
-    aws cloudformation update-stack --parameters ParameterKey=imageurl,ParameterValue=${DOCKER_IMAGE}:${TAG} --stack-name ${CF_STACK_NAME} --template-body file://cf/template.yml --capabilities CAPABILITY_IAM
-else
-    aws cloudformation create-stack --parameters ParameterKey=imageurl,ParameterValue=${DOCKER_IMAGE}:${TAG} --stack-name ${CF_STACK_NAME} --template-body file://cf/template.yml --capabilities CAPABILITY_IAM
-fi
+aws cloudformation deploy --parameter-overrides ParameterKey=imageurl,ParameterValue=${DOCKER_IMAGE}:${TAG} --stack-name ${CF_STACK_NAME} --template-body file://cf/template.yml --capabilities CAPABILITY_IAM
 
