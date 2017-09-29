f57ccd9040c84b4ac8abeb8bd728b5cce03bc53a
Fabian van der Werf <fvanderwerf@gmail.com>
2017-09-29 09:28:52 +0200

    mount aws credentials in dev container

--- bin/rundev.sh
+++ bin/rundev.sh
@@ -4,6 +4,6 @@ topdir=$(realpath "$(dirname "$0")/..")
 
 docker build -t dukascopy-dev ${topdir}/dev-docker
 
-docker run --rm -ti -v ${topdir}:/app dukascopy-dev /bin/sh
+docker run --rm -ti -v ${topdir}:/app -v${HOME}/.aws:/root/.aws dukascopy-dev /bin/sh
 
 
